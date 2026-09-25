b5b5f8afaabae8c1a9d9b1894cd148ea7e54eb4a
savgoran <savgoran@gmail.com>
2026-09-25 20:21:15 +0200

    Add experimental ScheduledSequence base

---
+++ src/ScheduledSequence.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AiSoft\ScheduledSequence;
+
+/**
+ * Experimental base type for persistent irregular schedules.
+ *
+ * The public API is intentionally incomplete while occurrence identity,
+ * dispatch semantics and reliability contracts are being refined.
+ */
+abstract class ScheduledSequence
+{
+}
